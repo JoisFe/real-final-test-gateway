3f18223995df5201bee17a336f24922c75cc87f6
조재철 <rhsnqk@gmail.com>
2022-09-17 23:40:18 +0900

    Spring Cloud Project with Maven

---
+++ real-final-test-gateway/src/main/java/com/nhnacademy/realfinaltestgateway/RealFinalTestGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.realfinaltestgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealFinalTestGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealFinalTestGatewayApplication.class, args);
+	}
+
+}
---
+++ real-final-test-gateway/src/test/java/com/nhnacademy/realfinaltestgateway/RealFinalTestGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.realfinaltestgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealFinalTestGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
